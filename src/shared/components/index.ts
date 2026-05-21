e47de9685b2b592e3d5da7af6d45c7d8e82f8be6
Mohammad Shaikh Ibrahim <m.s.ibrahim@alphaomega-eng.com>
2026-05-21 14:34:56 +0300

    feat: add SectionCard component for reusable UI sections

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,3 +1,5 @@
 export { Button } from './Button';
 export type { ButtonProps } from './Button';
+export { SectionCard } from './SectionCard';
+export type { SectionCardProps } from './SectionCard';
 export { ScreenHeader } from './ScreenHeader';
